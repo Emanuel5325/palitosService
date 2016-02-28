b129daed345ecd158a7853a500e93a4f27523d36
Vasco Pastor <vasquitoazcoitia@hotmail.com>
2016-02-28 01:05:08 -0300

    servicio WebService XML

---
+++ WebSite1/App_Code/MyServiceClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for ServiceClass
+/// </summary>
+
+[WebService]
+public class MyServiceClass
+{
+    public MyServiceClass()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    [WebMethod]
+    public string SayHello(string userName)
+    {
+
+        return string.Format("hello {0}", userName);
+
+    }
+}
